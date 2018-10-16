8f0d1f0b2abfd96c6b20f742596dbbb0b71eef0d
Daniel <dcthatch@gmail.com>
2018-10-16 23:35:20 +0100

    Use betterlockscreen if installed

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Check for betterlockscreen script
+which betterlockscreen && betterlockscreen -l && exit 0
+
+# Fall back to i3lock
 if [ -f $HOME/.i3/wallpaper.png ]; then 
     i3lock -i $HOME/.i3/wallpaper.png -u
 else
